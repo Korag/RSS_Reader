d54a5ccd3d6a10d4b133511c53a0048f7937c0ee
Kamil Hareza <kml.hareza@gmail.com>
2019-02-24 19:22:02 +0100

    Add saving to database inside OnTime method

--- RSS_Downloader/DownloaderService.cs
+++ RSS_Downloader/DownloaderService.cs
@@ -4,13 +4,17 @@ namespace RSS_Downloader
 {
     public partial class DownloaderService : ServiceBase
     {
+        private WebSiteContentDownloader _downloader;
+        private RssDocumentsRepository _context;
         public DownloaderService()
         {
             InitializeComponent();
+            _downloader = new WebSiteContentDownloader("https://www.rmf24.pl/kanaly/rss");
         }
 
         protected override void OnStart(string[] args)
         {
+
             System.Timers.Timer timer = new System.Timers.Timer();
             timer.Interval = 60000;
             timer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimer);
@@ -19,7 +23,12 @@ namespace RSS_Downloader
 
         public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
         {
-            
+            var rssDocuments = _downloader.GetContentFromWebSite();
+            foreach (var document in rssDocuments)
+            {
+                _context.SaveRssDocumentToDatabase(document);
+            }
+
         }
 
         protected override void OnStop()
