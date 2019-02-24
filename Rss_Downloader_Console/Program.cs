888cb1d44a2f7484d822c1b6299aeb91863e8244
Kamil Hareza <kml.hareza@gmail.com>
2019-02-24 20:37:17 +0100

    Change Main method logic to smaller methods

--- Rss_Downloader_Console/Program.cs
+++ Rss_Downloader_Console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Rss_Downloader.Db_Context;
+using Rss_Downloader.Models;
 using Rss_Downloader.Services;
 using System;
+using System.Collections.Generic;
 
 namespace Rss_Downloader
 {
@@ -8,35 +10,49 @@ namespace Rss_Downloader
     {
         private static WebSiteContentDownloader _downloader;
         private static RssDocumentsRepository _context;
-
+        private static List<RSSDocumentSingle> _rssDocuments;
 
         private static void Main(string[] args)
         {
             _downloader = new WebSiteContentDownloader("https://www.rmf24.pl/kanaly/rss");
             _context = new RssDocumentsRepository();
 
+            SaveDocumentSingleToDatabase();
+
             var startTimeSpan = TimeSpan.Zero;
-            var periodTimeSpan = TimeSpan.FromMinutes(1);
+            var periodTimeSpan = TimeSpan.FromMinutes(1);//1min
 
             var timer = new System.Threading.Timer((e) =>
             {
-                Download();
+                CheckIfNewContentIsAvailable();
             }, null, startTimeSpan, periodTimeSpan);
+
             Console.Read();
         }
-        public static void Download()
+        public static void SaveDocumentSingleToDatabase()
         {
-            var rssDocuments = _downloader.GetContentFromWebSite();
-            foreach (var document in rssDocuments)
+            _rssDocuments = _downloader.GetContentFromWebSite();
+            foreach (var content in _rssDocuments)
             {
+                _context.SaveRssDocumentToDatabase(content);
+            }
+        }
 
-                foreach (var content in rssDocuments)
-                {
-                    _downloader.GetSubContentOfMainSite(content);
-                }
-                _context.SaveRssDocumentToDatabase(document);
+        public static void SaveDocumentItemsToDatabase()
+        {
+            foreach (var content in _rssDocuments)
+            {
+                _downloader.GetSubContentOfMainSite(content);
+                _context.SaveRssDocumentToDatabase(content);
             }
 
+        }
+        public static void CheckIfNewContentIsAvailable()
+        {
+            if (true)
+            {
+                SaveDocumentItemsToDatabase();
+            }
         }
     }
 }
