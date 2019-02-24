4857a552c73e82b1e4c11e5b47668a9847e593a7
Korag <koragrex@gmail.com>
2019-02-24 18:35:12 +0100

    Added RssDocumentItem model

---
+++ RSS_Downloader/Models/RssDocumentItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS_Downloader.Models
+{
+    public class RssDocumentItem
+    {
+        public string Title { get; set; }
+        public string PubDate { get; set; }
+        public string Description { get; set; }
+    }
+}
