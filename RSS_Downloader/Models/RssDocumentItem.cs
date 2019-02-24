6d6224e5c8b0143e0c9689c898e4a5af0a12ca87
Korag <koragrex@gmail.com>
2019-02-24 18:54:21 +0100

    Refactorization of RssDocumentItem Model

--- RSS_Downloader/Models/RssDocumentItem.cs
+++ RSS_Downloader/Models/RssDocumentItem.cs
@@ -9,7 +9,10 @@ namespace RSS_Downloader.Models
     public class RssDocumentItem
     {
         public string Title { get; set; }
-        public string PubDate { get; set; }
         public string Description { get; set; }
+        public object Image { get; set; }
+        public string Links { get; set; }
+        public string DateOfPublication { get; set; }
+        public string Category { get; set; }
     }
 }
