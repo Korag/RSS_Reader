e0d142fae1dcda3cb1d71b86442267018e6f3fed
Korag <koragrex@gmail.com>
2019-02-25 18:25:07 +0100

    Created class used for Db Connection to mlab cloud database (MongoDB)

---
+++ RSS_Reader/Db_Context/RssDocumentsRepository.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using Rss_Reader.Models;
+using System.IO;
+using System.Linq;
+
+namespace Rss_Reader.Db_Context
+{
+    public class RssDocumentsRepository : IRssDocumentsRepository
+    {
+        private IMongoCollection<RSSDocumentSingle> _rssDocumentCollection;
+
+        public RssDocumentsRepository()
+        {
+            string user = "RssReader";
+            string password = "Reader1";
+            string database = "rss_downloader_web_application";
+            string connectionstring = $"mongodb://{user}:{password}@ds062818.mlab.com:62818/{database}";
+            string collectionRSS = "Stored_RSS";
+
+            var client = new MongoClient(connectionstring);
+            var server = client.GetDatabase(database);
+            _rssDocumentCollection = server.GetCollection<RSSDocumentSingle>(collectionRSS);
+        }
+
+        public IMongoCollection<RSSDocumentSingle> GetAllRssDocumentsFromDatabase()
+        {
+            return _rssDocumentCollection;
+        }
+
+
+    }
+}
