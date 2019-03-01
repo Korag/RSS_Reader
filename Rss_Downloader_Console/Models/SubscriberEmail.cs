c518250d91a0c2f9211a03d809f1457d800e049f
Łukasz Czepielik <koragrex@gmail.com>
2019-03-01 18:47:42 +0100

    Changed class Subscriber Email to public.

--- Rss_Downloader_Console/Models/SubscriberEmail.cs
+++ Rss_Downloader_Console/Models/SubscriberEmail.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Rss_Downloader_Console.Models
 {
-    class SubscriberEmail : Email
+    public class SubscriberEmail : Email
     {
         public string EmailAddress { get; set; }
         public List<RSSDocumentSingle> SubscriberList { get; set; }
