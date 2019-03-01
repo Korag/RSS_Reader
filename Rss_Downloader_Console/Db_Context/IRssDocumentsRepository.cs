16f1f491d5bcb69c2790ad80877705aeb26783f4
Łukasz Czepielik <koragrex@gmail.com>
2019-03-01 19:13:45 +0100

    Added new functions to Db Context interface (InitializeContext and GetSubscribersList).

--- Rss_Downloader_Console/Db_Context/IRssDocumentsRepository.cs
+++ Rss_Downloader_Console/Db_Context/IRssDocumentsRepository.cs
@@ -1,16 +1,22 @@
 ﻿
+using MongoDB.Driver;
 using Rss_Downloader.Models;
+using Rss_Downloader_Console.Models;
 using System.Collections.Generic;
 
 namespace Rss_Downloader.Db_Context
 {
     public interface IRssDocumentsRepository
     {
+        void InitializeContext(string connectionstring, string database);
         void SaveOneRssDocumentToDatabase(RSSDocumentSingle rssDocument);
         void SaveManyRssDocumentsToDatabase(List<RSSDocumentSingle> rssDocuments);
         bool CheckIfDatabaseIsEmpty();
         bool CheckIfDatabaseContainsEnoughDocuments();
         void DeleteAllDocumentsFromDatabse();
+        IMongoCollection<SubscriberEmail> GetSubscribersList();
+
+
 
     }
 }
