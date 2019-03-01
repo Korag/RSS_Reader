0f0a2327cd786633bf802edbff9650e94eab71df
Łukasz Czepielik <koragrex@gmail.com>
2019-03-01 17:25:24 +0100

    Added InitializeContext method, small refactoring

--- Rss_Downloader_Console/Db_Context/RssDocumentsRepository.cs
+++ Rss_Downloader_Console/Db_Context/RssDocumentsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using Rss_Downloader.Models;
+using Rss_Downloader_Console.Models;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,6 +11,11 @@ namespace Rss_Downloader.Db_Context
     {
         private IMongoCollection<RSSDocumentSingle> _rssDocumentCollection;
 
+        private MongoClient _mongoClient;
+        private IMongoDatabase _server;
+        public string collectionRSS = "Stored_RSS";
+
+
         public RssDocumentsRepository()
         {
             string path = @"C:\credentials.txt";
@@ -17,11 +23,15 @@ namespace Rss_Downloader.Db_Context
             string password = File.ReadLines(path).First();
             string database = "rss_downloader_web_application";
             string connectionstring = $"mongodb://{user}:{password}@ds062818.mlab.com:62818/{database}";
-            string collectionRSS = "Stored_RSS";
 
-            var client = new MongoClient(connectionstring);
-            var server = client.GetDatabase(database);
-            _rssDocumentCollection = server.GetCollection<RSSDocumentSingle>(collectionRSS);
+            InitializeContext(connectionstring, database);
+        }
+
+        public void InitializeContext(string connectionstring, string database)
+        {
+            _mongoClient = new MongoClient(connectionstring);
+            _server = _mongoClient.GetDatabase(database);
+            _rssDocumentCollection = _server.GetCollection<RSSDocumentSingle>(collectionRSS);
         }
 
         public void SaveOneRssDocumentToDatabase(RSSDocumentSingle rssDocument)
