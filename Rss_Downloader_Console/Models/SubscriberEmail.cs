b22105b029150d90d79b5190c2fbe67faf234345
Łukasz Czepielik <koragrex@gmail.com>
2019-03-01 15:36:26 +0100

    Added model which defines the structure of Subscribers data stored in database

---
+++ Rss_Downloader_Console/Models/SubscriberEmail.cs
@@ -0,0 +1,12 @@
+﻿using Postal;
+using Rss_Downloader.Models;
+using System.Collections.Generic;
+
+namespace Rss_Downloader_Console.Models
+{
+    class SubscriberEmail : Email
+    {
+        public string EmailAddress { get; set; }
+        public List<RSSDocumentSingle> SubscriberList { get; set; }
+    }
+}
