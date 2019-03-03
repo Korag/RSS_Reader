bee099ee10deddca8e9c7a2893b80d151eb83dac
Kamil Hareza <kml.hareza@gmail.com>
2019-03-03 14:52:56 +0100

    Added reader controller and view

---
+++ RSS_Reader/Controllers/ReaderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSS_Reader.Controllers
+{
+    public class ReaderController : Controller
+    {
+        // GET: Reader
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
