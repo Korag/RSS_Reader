42fcc59192bc6f10bc3278780569ee47d083f84f
Łukasz Czepielik <koragrex@gmail.com>
2019-03-09 16:21:39 +0100

    Added ID Bson property to SubscriberEmail Model.

--- RssModelsLib/Models/SubscriberEmail.cs
+++ RssModelsLib/Models/SubscriberEmail.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
 
 namespace RssModelsLib.Models
 {
     public class SubscriberEmail
     {
+        [BsonId]
+        public ObjectId Id { get; set; }
         public string EmailAddress { get; set; }
         public List<string> SubscriberList { get; set; }
     }
