2a0233ff8661aa5353baddab2390b177ec920985
Kamil Hareza <kml.hareza@gmail.com>
2019-03-10 17:28:22 +0100

    Added unit test project for Rss_Downloader

---
+++ Rss_Downloader.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Rss_Downloader.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
