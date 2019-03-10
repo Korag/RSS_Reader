740c4b2b37477905b7fcd95e8a1e542c0bd5aa34
Kamil Hareza <kml.hareza@gmail.com>
2019-03-10 18:01:21 +0100

    Added tests for GetAllDocumentsWithoutSubContent method

--- Rss_Downloader.UnitTests/WebSiteContentDownloaderTests.cs
+++ Rss_Downloader.UnitTests/WebSiteContentDownloaderTests.cs
@@ -1,13 +1,42 @@
 ﻿using NUnit.Framework;
+using Rss_Downloader.Services;
+using RssModelsLib.Models;
+using System.Collections.Generic;
 
 namespace Rss_Downloader.UnitTests
 {
     [TestFixture]
     public class WebSiteContentDownloaderTests
     {
-        [Test]
-        public void TestMethod1()
+        private IWebSiteContentDownloader _downloader;
+        [SetUp]
+        public void SetUp()
         {
+            _downloader = new WebSiteContentDownloader();
+        }
+
+        [Test]
+        public void GetAllDocumentsWithoutSubContent_WhenCalled_ReturnedListIsNotEmpty()
+        {
+            var result = _downloader.GetAllDocumentsWithoutSubContent();
+
+            Assert.That(result, Is.Not.Empty);
+        }
+
+        [Test]
+        public void GetAllDocumentsWithoutSubContent_WhenCalled_ReturnedListIsNotNull()
+        {
+            var result = _downloader.GetAllDocumentsWithoutSubContent();
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void GetAllDocumentsWithoutSubContent_WhenCalled_ReturnedListContainsRssDocumentSingleObjects()
+        {
+            var result = _downloader.GetAllDocumentsWithoutSubContent();
+
+            Assert.That(result, Is.InstanceOf(typeof(List<RssDocumentSingle>)));
         }
     }
 }
