8ba32ab3a66c834f16589e20580adb967e558317
Kamil Hareza <kml.hareza@gmail.com>
2019-03-10 17:17:58 +0100

    Added new class and moved logic from Main method to DownloaderMainLogic class

---
+++ Rss_Downloader_Console/DownloaderMainLogic.cs
@@ -0,0 +1,69 @@
+﻿using Rss_Downloader.Services;
+using Rss_Downloader_Console.Services;
+using RssDbContextLib.Db_Context;
+using RssModelsLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rss_Downloader_Console
+{
+
+
+    public class DownloaderMainLogic : IDownloaderMainLogic
+    {
+        private WebSiteContentDownloader _downloader;
+        private RssDocumentsRepository _context;
+        private EmailServiceProvider _emailProvider;
+        private List<RssDocumentSingle> _rssDocuments;
+
+        public DownloaderMainLogic()
+        {
+            _downloader = new WebSiteContentDownloader("https://www.rmf24.pl/kanaly/rss");
+            _context = new RssDocumentsRepository();
+
+            if (_context.CheckIfDatabaseIsEmpty())
+            {
+                SaveDocumentSingleToDatabase();
+                Console.WriteLine("Downloading finished");
+            }
+        }
+        public void SaveDocumentSingleToDatabase()
+        {
+            _rssDocuments = _downloader.GetAllDocumentsWithoutSubContent();
+            Parallel.ForEach(_rssDocuments, (content) =>
+            {
+                _downloader.FillSingleDocumentWithSubContent(content);
+            });
+            _context.SaveManyRssDocumentsToDatabase(_rssDocuments);
+        }
+
+        public List<RssDocumentSingle> GetDocumentsWithNewContent()
+        {
+            var documentsFromDb = _context.GetAllDocuments();
+            return _downloader.GetDocumentsWithNewContentAvailable(documentsFromDb);
+        }
+
+        public void DownloadNewContentIfItsAvailable()
+        {
+            var newContent = GetDocumentsWithNewContent();
+            if (newContent.Count > 0)
+            {
+                Console.WriteLine("New content is available");
+
+                var documentsWithNewContent = _downloader.AddNewContentToDocuments(newContent);
+                _context.AddNewContent(documentsWithNewContent);
+
+                Console.WriteLine("New content has been added to: ");
+                foreach (var document in documentsWithNewContent)
+                {
+                    Console.WriteLine(document.Title);
+                }
+            }
+            else
+            {
+                Console.WriteLine("New content is not available ");
+            }
+        }
+    }
+}
---
+++ Rss_Downloader_Console/IDownloaderMainLogic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using RssModelsLib.Models;
+
+namespace Rss_Downloader_Console
+{
+    public interface IDownloaderMainLogic
+    {
+        void DownloadNewContentIfItsAvailable();
+        List<RssDocumentSingle> GetDocumentsWithNewContent();
+        void SaveDocumentSingleToDatabase();
+    }
+}
