545489267c4d4b2c92d5596458bddfb16f222cc1
Kamil Hareza <kml.hareza@gmail.com>
2019-03-22 16:55:00 +0100

    Added Autofac basic configurations for downloader
    Registers basic downloader types and interfaces

---
+++ Rss_Downloader_Console/IoC/ContainerConfig.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Rss_Downloader.Services;
+using RssDbContextLib.Db_Context;
+
+namespace Rss_Downloader_Console.IoC
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DownloaderMainLogic>().As<IDownloaderMainLogic>();
+            builder.RegisterType<WebSiteContentDownloader>().As<IWebSiteContentDownloader>();
+            builder.RegisterType<RssDocumentsRepository>().As<IRssDocumentsRepository>();
+
+            return builder.Build();
+        }
+    }
+}
