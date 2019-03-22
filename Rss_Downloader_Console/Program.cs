4b0520b209d0b3230bc3c7fe08352f519f810100
Kamil Hareza <kml.hareza@gmail.com>
2019-03-22 17:16:04 +0100

    Added Autofac container build and application start to Main method

--- Rss_Downloader_Console/Program.cs
+++ Rss_Downloader_Console/Program.cs
@@ -1,29 +1,22 @@
-﻿using Rss_Downloader_Console;
+﻿using Autofac;
+using Rss_Downloader_Console;
+using Rss_Downloader_Console.IoC;
 
 namespace Rss_Downloader
 {
     internal class Program
     {
-        private static IDownloaderMainLogic _downloader;
         private static void Main(string[] args)
         {
-            _downloader = new DownloaderMainLogic();
 
-            #region Local
-            //var startTimeSpan = TimeSpan.Zero;
-            //var periodTimeSpan = TimeSpan.FromMinutes(1);//1min
+            var container = ContainerConfig.Configure();
 
-            //var timer = new System.Threading.Timer((e) =>
-            //{
-            //    _downloader.DownloadNewContentIfItsAvailable();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplicationStarter>();
+                app.Run();
 
-            //}, null, startTimeSpan, periodTimeSpan);
-            #endregion
-
-            #region Azure
-            _downloader.DownloadNewContentIfItsAvailable();
-            #endregion
-
+            }
         }
     }
 }
